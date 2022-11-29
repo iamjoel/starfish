cb39ce69ebcb122e405f1e4f464e8bf694011cd8
金伟强 <joel@metasecurelabs.io>
2022-11-29 15:51:14 +0800

    feat: transfer

--- code/playground/src/views/connect-wallet/index.js
+++ code/playground/src/views/connect-wallet/index.js
@@ -2,8 +2,11 @@ import {useState, useEffect} from 'react'
 import { ethers } from 'ethers'
 import connectToMetamask from '@/utils/connect-to-meta-mask'
 
+// Metamask 系列 https://initstack.cn/posts/metamask-intro-4/
 const ethereum = window.ethereum
 const toChainId = '0x1' // 0x38: 币安链;0x43114。https://chainlist.org/zh
+
+const toAddress = '0x7540D7Ea64cD21c683DbD0389DF8F0bAe29BC7Bf'
 
 function App() {
   const [signer, setSigner] = useState(null)
@@ -34,6 +37,27 @@ function App() {
     } else {
         doConnect()
     }
+  }
+
+  // 转账。
+  const sendEth = async () => {
+      if(address.toLowerCase() === toAddress.toLowerCase()) {
+          alert('send to self is not allowed')
+          return
+      }
+      const txHash = await ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [
+            {
+                from: address, // 当前的用户钱包
+                to: toAddress, // 转账目标地址
+                value: '0x100', // 转账数量，单位为该币种最小单位（如ETH中的wei），注意，是16进制的
+            },
+        ],
+      })
+        //   https://goerli.etherscan.io/tx/0x7249c0360b327d92640041e131a722ef4141533ad2cfa6f313cb29cb99d364d0
+      console.log(txHash) // 交易id
+      alert('转账成功')
   }
 
   const doConnect = async (notSwitch) => {
@@ -91,6 +115,8 @@ function App() {
       {
           address && <div>address: {address}</div>
       }
+      <button onClick={sendEth}>转账</button>
+      
     </div>
   );
 }
